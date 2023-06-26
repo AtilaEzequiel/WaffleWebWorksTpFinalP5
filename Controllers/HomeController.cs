04c809aa7d61f16bfc7655733002beb255a8e446
AtilaEzequiel <112121260+AtilaEzequiel@users.noreply.github.com>
2023-06-26 12:17:34 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,313 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WaffleWebWorksTpFinal.Models;
+// usibg
+using Microsoft.Data.SqlClient;
+using System.Security;
+using System;
+
+namespace WaffleWebWorksTpFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MovieADO;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        // guardo lo necesario para conectar a la base de datos en un string para despues pegar el string y listo
+        List<Carrera> list = new List<Carrera>();
+        List<CarreraIma> listima = new List<CarreraIma>();
+        //lisrta para guardar los resultados del select
+        public IActionResult Index()
+        {
+            try
+            {
+                //prepara la conectar
+                SqlConnection connection = new SqlConnection(connectionString);
+                //abre la coencta
+                connection.Open();
+                // guarda en un string el codigo sql a ejecutar
+                //string queryString = "Select * from Carrera";
+                string queryString = "Select * from CarreraImagen";
+                //string queryString = "INSERT INTO MovieADO (Id, titulo, fecha, genero, precio) VALUES (10, 'Delta', 15/12/1999, 'magia', 600);";
+                // no me acurdo, creoq ue guarda en un comadno sql lo que debe ejecutar y en que conexion hacerlo
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //  command.ExecuteReader(queryString);
+                //command.Parameters.AddWithValue("@Id", id);
+                //ejecuta el codigo sql y guarda en reader
+                SqlDataReader reader = command.ExecuteReader();
+
+                //repite las filas obtenidas
+                while (reader.Read())
+                {
+                    //String nameimagen = reader[3].ToString();
+
+                    // nameimagen = nameimagen.Replace("https://drive.google.com/file/d/", "/view?usp=sharing");
+                  //  nameimagen = "https://drive.google.com/uc?export=view&id=" + nameimagen;
+                    CarreraIma movieADOs = new CarreraIma()
+                    {
+                        //guarda por elemento del modelo carrera
+                        Id = int.Parse(reader[0].ToString()),
+                        Name = reader[1].ToString(),
+                        //
+                        //ReleaseDate = DateTime.Parse(reader[2].ToString()),
+                        Description = reader[2].ToString(),
+                        Imagen =  reader[3].ToString(),
+                        //Imagen = reader[3].ToString()
+                        
+                    //  Price = int.Parse(reader[4].ToString()),
+                };
+                    //guarda en la lista
+                    listima.Add(movieADOs);
+                  //  list.Add(movieADOs);
+                    // return View(movieADOs);
+
+                    //  list.Add(movieADOs);
+                    //list.Add(movieADOs1);
+                }
+
+
+                //cierrra la conexion
+                connection.Close();
+                //muestra la lista de resutltado
+                return View(listima);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CarreraIma mov)
+        {
+            try
+            {
+
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                connection.Open();
+
+                string queryString = "INSERT INTO CarreraImagen (carrera, Descripcion, nombreimagen) VALUES ( @Carrera, @Description, @nombreimagen);";
+                //string queryString = "INSERT INTO MovieADO (Id, titulo, fecha, genero, precio) VALUES (10, 'Delta', 15/12/1999, 'magia', 600);";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //  command.ExecuteReader(queryString);
+                //toma los parametros obtenidos para despues agregarlos en el consulta sql
+                command.Parameters.AddWithValue("@Carrera", mov.Name);
+                command.Parameters.AddWithValue("@Description", mov.Description);
+                String nameimagen = mov.Imagen;
+
+                
+                //nameimagen = nameimagen.Replace("/view?usp=sharing/","");
+               
+                command.Parameters.AddWithValue("@nombreimagen", nameimagen);
+
+                
+
+                //ejecuta la consulta
+                SqlDataReader reader = command.ExecuteReader();
+                
+
+                connection.Close();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public ActionResult Detalle(int id)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                //filtra por id
+                string queryString = "Select * from CarreraImagen Where Id=@ID";
+                //string queryString = "INSERT INTO carrera (Id, titulo, fecha, genero, precio) VALUES (10, 'Delta', 15/12/1999, 'magia', 600);";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //  command.ExecuteReader(queryString);
+                //necesario hacer este para que sepa a que id filtrar
+                command.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    CarreraIma movieADOs = new CarreraIma()
+                    {
+                        Id = int.Parse(reader[0].ToString()),
+                        Name = reader[1].ToString(),
+                        //
+                        //ReleaseDate = DateTime.Parse(reader[2].ToString()),
+                        Description = reader[2].ToString(),
+                        Imagen = reader[3].ToString(),
+                    };
+                    return View(movieADOs);
+
+                    //  list.Add(movieADOs);
+                    //list.Add(movieADOs1);
+                }
+
+
+
+                connection.Close();
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //esto seria editor pero el editor sin httpost es lo mismo que el de arriba. ademas un poco de variedad
+        public async Task<IActionResult> Detalle(int id, CarreraIma mov)
+        {
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                string queryString = "update CarreraImagen   set  carrera=@Name, Descripcion=@Description, nombreimagen=@nombreimagen  where Id = @id";
+                //string queryString = "INSERT INTO MovieADO (Id, titulo, fecha, genero, precio) VALUES (10, 'Delta', 15/12/1999, 'magia', 600);";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //  command.ExecuteReader(queryString);
+                command.Parameters.AddWithValue("@Id", id);
+                command.Parameters.AddWithValue("@Name", mov.Name);
+                command.Parameters.AddWithValue("@Description", mov.Description);
+
+                String nameimagen = mov.Imagen;
+
+                nameimagen = nameimagen.Replace("https://drive.google.com/file/d/", "");
+                nameimagen = nameimagen.Replace("/view?usp=sharing", "");
+                //nameimagen = nameimagen.Replace("/view?usp=sharing/","");
+
+                command.Parameters.AddWithValue("@nombreimagen", nameimagen);
+
+
+
+
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                connection.Close();
+
+                return RedirectToAction("Detalle");
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+        // intente hacer lo mismo que arriba pero no pude, la razon nose 
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                string queryString = "Select * from CarreraImagen Where Id=@ID";
+                //string queryString = "INSERT INTO carrera (Id, titulo, fecha, genero, precio) VALUES (10, 'Delta', 15/12/1999, 'magia', 600);";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //  command.ExecuteReader(queryString);
+                command.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    CarreraIma movieADOs = new CarreraIma()
+                    {
+                        Id = int.Parse(reader[0].ToString()),
+                        Name = reader[1].ToString(),
+                        //
+                        //ReleaseDate = DateTime.Parse(reader[2].ToString()),
+                        Description = reader[2].ToString(),
+                        Imagen = reader[3].ToString()
+                        
+                    };
+                    return View(movieADOs);
+
+                    //  list.Add(movieADOs);
+                    //list.Add(movieADOs1);
+                }
+
+
+
+                connection.Close();
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete( CarreraIma mov)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                string queryString = "delete from CarreraImagen where Id = @Id";
+                //string queryString = "INSERT INTO MovieADO (Id, titulo, fecha, genero, precio) VALUES (10, 'Delta', 15/12/1999, 'magia', 600);";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //  command.ExecuteReader(queryString);
+                command.Parameters.AddWithValue("@Id", mov.Id);
+
+
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                connection.Close();
+
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/CarreraIma.cs
@@ -0,0 +1,11 @@
+﻿namespace WaffleWebWorksTpFinal.Models
+{
+    public class CarreraIma
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Imagen { get; set;}
+        // public byte[] Imagen { get; set; }
+    }
+}
